8c42366040401fd888eff37fc835fa2c269260da
Roshan Kenia <keniaroshan@gmail.com>
2022-11-20 14:45:55 -0500

    Update consistencyTraining.py

--- consistencyTraining.py
+++ consistencyTraining.py
@@ -146,10 +146,8 @@ def train(epoch, train_loader, model, optimizer, num_classes, noise_or_not, trai
         print('pseudo:', unconf_pseudolabels)
 
         # heavily augment images
-        print('unconf act ind:',
-              indexes[sum_unconfident_ind].to(dtype=torch.long))
-        aug_images, aug_lab, aug_ind = train_dataset.getItemRandAug(
-            index=indexes[sum_unconfident_ind].to(dtype=torch.long))
+        print('unconf act ind:', indexes[sum_unconfident_ind])
+        aug_images, aug_lab, aug_ind = train_dataset.getItemRandAug(indexes)
         aug_images = Variable(aug_images).cuda()
 
         print('images unconf 1:', aug_images)
