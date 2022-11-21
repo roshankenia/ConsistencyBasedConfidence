2ada9a6798cbc3c5aa326a6c04a9c38081e616ad
Roshan Kenia <keniaroshan@gmail.com>
2022-11-20 19:23:07 -0500

    Update consTrainNoMixUp.py

--- consTrainNoMixUp.py
+++ consTrainNoMixUp.py
@@ -62,28 +62,6 @@ def accuracy(logit, target, topk=(1,)):
     return res
 
 # Train the Model
-
-
-def mixup_data(x, y, alpha=1.0, use_cuda=True):
-    '''Returns mixed inputs, pairs of targets, and lambda'''
-    if alpha > 0:
-        lam = np.random.beta(alpha, alpha)
-    else:
-        lam = 1
-
-    lam = max(lam, 1-lam)
-
-    batch_size = x.size()[0]
-    if use_cuda:
-        index = torch.randperm(batch_size).cuda()
-    else:
-        index = torch.randperm(batch_size)
-
-    mixed_x = lam * x + (1 - lam) * x[index, :]
-    y_a, y_b = y, y[index]
-    return mixed_x, y_a, y_b, lam
-
-
 def train(epoch, train_loader, model, optimizer):
     train_total = 0
     train_correct = 0
